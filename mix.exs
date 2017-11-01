2cb4a3cd8d2fcec009c5dffb70cabc060e797ed8
Dmitry Trager <dmitry.trager@flatstack.com>
2017-11-02 01:58:59 +0300

    Update version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule SentinelApi.Mixfile do
 
   def project do
     [app: :sentinel_api,
-     version: "0.1.3",
+     version: "0.1.4",
      elixir: "~> 1.5",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
