f6af45735856c136f01b299689e33d33e5e7a151
Dmitry Trager <dmitry.trager@flatstack.com>
2017-11-01 13:30:48 +0300

    Add predict script and logic to start prediction

--- lib/sentinel_api/image_classification.ex
+++ lib/sentinel_api/image_classification.ex
@@ -2,23 +2,35 @@ defmodule SentinelApi.ImageClassification do
   alias SentinelApi.Image
 
   def start(%{"image" => image, "callback_uri" => callback_uri, "name" => name}) do
-    case Image.store({image, name}) do
-      {:ok, filename} ->
-        IO.puts(filename)
+    case name do
+      "_" -> start_prediction
+      _ ->
+        case Image.store({image, name}) do
+          {:ok, filename} ->
+            IO.puts(filename)
 
-        start_learning
-        {:ok, filename}
-      {:error, reason} ->
-        IO.puts(reason)
+            # start_learning
+            {:ok, filename}
+          {:error, reason} ->
+            IO.puts(reason)
 
-        {:error, reason}
+            {:error, reason}
+        end
     end
   end
 
   defp start_learning do
     System.cmd(
       "#{priv_dir}/learn",
-      [4, "/root/openface", "#{app_dir}/uploads/users", "#{app_dir}/uploads/users_aligned", "#{app_dir}/uploads/feature"],
+      [4, "~/openface", "#{app_dir}/uploads/users", "#{app_dir}/uploads/users_aligned"],
+      stderr_to_stdout: true
+    )
+  end
+
+  defp start_prediction do
+    System.cmd(
+      "#{priv_dir}/predict",
+      ["~/openface", "#{app_dir}/uploads/users"],
       stderr_to_stdout: true
     )
   end
